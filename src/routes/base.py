3e68b2dfeb44f07809f85bd5d3791e44e5df92a1
Muhammed Younes <157610027+myounes21@users.noreply.github.com>
2026-09-25 23:36:05 +0300

    feat: rename router to base_router and simplify welcome endpoint

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,18 +1,16 @@
 from fastapi import APIRouter
 from helpers.config import settings
 
-router = APIRouter(
+base_router = APIRouter(
     prefix="/api/v1",
     tags=["api_v1"]
 )
 
-@router.get("/")
+@base_router.get("/")
 async def welcome():
 
-    app_name = settings.APP_NAME
-    app_version = settings.APP_VERSION
-
     return {
-        "name": app_name,
-        "version": app_version
+        "name": settings.APP_NAME,
+        "version":  settings.APP_VERSION
+
     }
